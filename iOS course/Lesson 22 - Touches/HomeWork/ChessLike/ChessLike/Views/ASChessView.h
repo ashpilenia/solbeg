85a9b44d67f3a6edb9e6de2db9c6c36c3f3b4ad1
Alex Shpilenia <ashpilenia@gmail.com>
2018-10-26 16:17:51 +0300

    completed homework Drag and Drop (lesson 22)

---
+++ iOS course/Lesson 22 - Touches/HomeWork/ChessLike/ChessLike/Views/ASChessView.h
@@ -0,0 +1,17 @@
+//
+//  ASChessView.h
+//  ChessLike
+//
+//  Created by Alex on 10/25/18.
+//  Copyright © 2018 Alex Shpilenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ASChessView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS course/Lesson 22 - Touches/HomeWork/ChessLike/ChessLike/Views/ASChessView.m
@@ -0,0 +1,21 @@
+//
+//  ASChessView.m
+//  ChessLike
+//
+//  Created by Alex on 10/25/18.
+//  Copyright © 2018 Alex Shpilenia. All rights reserved.
+//
+
+#import "ASChessView.h"
+
+@implementation ASChessView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
