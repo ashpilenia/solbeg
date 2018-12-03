222c1736a2d15d9ca2d36a3d8288e25de9ae5e6c
Alex Shpilenia <ashpilenia@gmail.com>
2018-12-03 16:02:47 +0300

    added UI for Calculator app

---
+++ iOS course/Lesson 25 - Buttons/ASCalculator/ASCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASCalculator
+//
+//  Created by Alex on 12/3/18.
+//  Copyright © 2018 Alex Shpilenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS course/Lesson 25 - Buttons/ASCalculator/ASCalculator/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  ASCalculator
+//
+//  Created by Alex on 12/3/18.
+//  Copyright © 2018 Alex Shpilenia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+
+@end
