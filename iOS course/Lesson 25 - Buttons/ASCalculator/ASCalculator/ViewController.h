304dbd28e5fb36e7c5e7a50516e69870689a50a8
Alex Shpilenia <ashpilenia@gmail.com>
2018-12-03 17:28:34 +0300

    commit

--- iOS course/Lesson 25 - Buttons/ASCalculator/ASCalculator/ViewController.h
+++ iOS course/Lesson 25 - Buttons/ASCalculator/ASCalculator/ViewController.h
@@ -10,6 +10,27 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
+@property (weak, nonatomic) IBOutlet UIButton *changeSignButton;
+@property (weak, nonatomic) IBOutlet UIButton *percantageButton;
+@property (weak, nonatomic) IBOutlet UIButton *divisionButton;
+@property (weak, nonatomic) IBOutlet UIButton *multiplierButton;
+@property (weak, nonatomic) IBOutlet UIButton *minusButton;
+@property (weak, nonatomic) IBOutlet UIButton *plusButton;
+@property (weak, nonatomic) IBOutlet UIButton *dotButton;
+@property (weak, nonatomic) IBOutlet UIButton *equalButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *sevenButton;
+@property (weak, nonatomic) IBOutlet UIButton *eightButton;
+@property (weak, nonatomic) IBOutlet UIButton *nineButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton;
+@property (weak, nonatomic) IBOutlet UIButton *fiveButton;
+@property (weak, nonatomic) IBOutlet UIButton *sixButton;
+@property (weak, nonatomic) IBOutlet UIButton *oneButton;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton;
+@property (weak, nonatomic) IBOutlet UIButton *zeroButton;
+
 
 @end
 
--- iOS course/Lesson 25 - Buttons/ASCalculator/ASCalculator/ViewController.m
+++ iOS course/Lesson 25 - Buttons/ASCalculator/ASCalculator/ViewController.m
@@ -17,6 +17,65 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    //set outlets properties
+    self.clearButton.layer.borderWidth = 0.5f;
+    self.clearButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.changeSignButton.layer.borderWidth = 0.5f;
+    self.changeSignButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.percantageButton.layer.borderWidth = 0.5f;
+    self.percantageButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.divisionButton.layer.borderWidth = 0.5f;
+    self.divisionButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.sevenButton.layer.borderWidth = 0.5f;
+    self.sevenButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.eightButton.layer.borderWidth = 0.5f;
+    self.eightButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.nineButton.layer.borderWidth = 0.5f;
+    self.nineButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.multiplierButton.layer.borderWidth = 0.5f;
+    self.multiplierButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.fourButton.layer.borderWidth = 0.5f;
+    self.fourButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.fiveButton.layer.borderWidth = 0.5f;
+    self.fiveButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.sixButton.layer.borderWidth = 0.5f;
+    self.sixButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.minusButton.layer.borderWidth = 0.5f;
+    self.minusButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    [self.oneButton.layer setBorderWidth:0.5f];
+    [self.oneButton.layer setBorderColor:[UIColor blackColor].CGColor];
+    
+    self.twoButton.layer.borderWidth = 0.5f;
+    self.twoButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.threeButton.layer.borderWidth = 0.5f;
+    self.threeButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.plusButton.layer.borderWidth = 0.5f;
+    self.plusButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.zeroButton.layer.borderWidth = 0.5f;
+    self.zeroButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.dotButton.layer.borderWidth = 0.5f;
+    self.dotButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    self.equalButton.layer.borderWidth = 0.5f;
+    self.equalButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+ 
 
 }
 
